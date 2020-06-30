3490c9bd48fd0763a149f113e87b26b8d28e4966
Prateek Chatterjee <pchatterjee136@gmail.com>
2020-06-30 13:01:10 +0530

    added build

---
+++ build/precache-manifest.58f69a14f62ef24379b1a6f2bc5a1820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d37c1206e83c0838c5524b76c1765d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "272144703d3d094ae945",
+    "url": "/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "be64c83b876f1507d952",
+    "url": "/static/js/2.1b457b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b457b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272144703d3d094ae945",
+    "url": "/static/js/main.78ac828a.chunk.js"
+  },
+  {
+    "revision": "4a29157b3354189a7920",
+    "url": "/static/js/runtime-main.3e20e9b1.js"
+  }
+]);
